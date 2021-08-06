43780b0f8dbfc68a292bfcbd3bd0fd0692178240
TheTerminatorOfProgramming <41417119+TheTerminatorOfProgramming@users.noreply.github.com>
2021-08-06 09:14:45 +0800

    removed project to replace with newer version

--- CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-# Changelog
-
-## [1.0.0] - 2021-08-05
-### Added
-- Initial version.
--- app/src/main/java/com/poupa/vinylmusicplayer/helper/M3UConstants.java
+++
@@ -1,8 +0,0 @@
-package com.poupa.vinylmusicplayer.helper;
-
-public interface M3UConstants {
-    String EXTENSION = "m3u";
-    String HEADER = "#EXTM3U";
-    String ENTRY = "#EXTINF:";
-    String DURATION_SEPARATOR = ",";
-}
